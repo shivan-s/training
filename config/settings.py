ef41f1f96f00cbbc58ae7355e132ac2e45a6932f
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2022-12-24 10:47:59 +1300

    wip

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@
 
 import os
 from pathlib import Path
-from urllib import urlparse
+from urllib.parse import urlparse
 
 import sentry_sdk
 from django.core.management.utils import get_random_secret_key
